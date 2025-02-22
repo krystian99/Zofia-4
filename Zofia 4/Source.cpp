7831a9b1d77daf88e3124b876c48375590bef617
skylerr091 <skylerr091@gmail.com>
2025-02-22 16:45:58 +0100

    dfhdfhd

--- Zofia 4/Source.cpp
+++ Zofia 4/Source.cpp
@@ -3,49 +3,21 @@
 
 using namespace std;
 
-const int MAX = 256;
-
-int tab[MAX]; // tablica wyzerowana
 
 int main()
 {
 	string str;
-
+	
 	int t;
-
 	cin >> t;
-	cin.get();
 
-	for (int i = 0; i < t; ++i)
+	getline(cin, str);
+
+
+	for (int i = 0; i < str.size(); i++)
 	{
-		getline(cin, str);
-
-		for (int j = 0; j < str.size(); j++)
-		{
-			char c = str[j];
-			tab[c]++;
-		}
-
-	}
-
-	/*for (int i = 97; i <= 122; ++i)
-	{
-		if (tab[i] > 0)
-			cout << char(i) << " " << tab[i] << endl;
-
-	}*/
-
-	for (char i = 'a'; i <= 'z'; ++i)
-	{
-		if (tab[i] > 0)	
-			cout << i << " " << tab[i] << endl;
-
-	}
-
-	for (char i = 'A'; i <= 'Z'; i++)
-	{
-		if (tab[i] > 0)
-			cout << i << " " << tab[i] << endl;
+		if (str[i] != ' ')
+			cout << str[i];
 	}
 
 	return 0;
